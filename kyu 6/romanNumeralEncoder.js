7783aff7d11cac4b0186849a9323fcb6dd8f3f31
theDreamer911 <thetrulydreamer@gmail.com>
2021-03-09 11:55:59 +0700

    roman numeral encode

---
+++ kyu 6/romanNumeralEncoder.js
@@ -0,0 +1,40 @@
+const romanEncoder = (num) => {
+  const roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  var str = "";
+  for (var i of Object.keys(roman)) {
+    var q = Math.floor(num / roman[i]);
+    num -= q * roman[i];
+    str += i.repeat(q);
+  }
+  return str;
+};
+
+console.log(romanEncoder(1500));
+
+// function solution(number){
+//     // convert the number to a roman numeral
+//   var  roman = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 }
+
+//   var ans = '';
+//   while(number>0){
+//       for(a in roman){
+//           if(roman[a]<=number){ ans += a; number-=roman[a]; break;}
+
+//       }
+//   }
+//   return ans;
+//   }
---
+++ kyu 8/feastOfManyBeasts.js
@@ -0,0 +1,4 @@
+const feast = (beast, dish) =>
+  beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+    ? true
+    : false;
